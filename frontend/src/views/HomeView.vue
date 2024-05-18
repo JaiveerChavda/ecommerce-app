7b8c87ac40dd798516e260af2d230a7cd777d127
jaiveer-truptman <jchavda@truptman.in>
2024-05-18 15:16:30 +0530

    install and setup vue + tailwind

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <h1 class="bg-red-500">Jaiho</h1>
+  </main>
+</template>
