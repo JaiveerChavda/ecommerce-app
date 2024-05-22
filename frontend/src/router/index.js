48e46ab82f16b1f0793d1af3751e1ccb24df8d2d
jaiveer-truptman <jchavda@truptman.in>
2024-05-22 17:54:41 +0530

    redirect to dashboard for default app

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,6 +14,14 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path:'/',
+            redirect: '/app/dashboard'
+        },
+        {
+            path:'/app',
+            redirect: '/app/dashboard'
+        },
+        {
             path: '/app',
             name: 'app',
             component: AppLayout,
