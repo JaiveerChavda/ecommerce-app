26b4e92fd77fadd108797d251666d90355f9bf1b
jaiveer-truptman <jchavda@truptman.in>
2024-05-24 16:04:17 +0530

    add core pop up modal and input field

---
+++ frontend/src/components/core/CustomInput.vue
@@ -0,0 +1,39 @@
+<script setup>
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'text',
+    },
+    name: String,
+    required: Boolean,
+    modelValue: String,
+    label:String
+})
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+<template>
+    <div>
+    <label class="sr-only text-black" > {{ label }}</label>
+    <template v-if="type === 'textarea'">
+        <textarea :name="name"
+            :required="required"
+            :value="props.modelValue"
+            @input="emit('update:modelValue', $event.target.value)"
+            class="block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            :placeholder="label">
+        </textarea>
+    </template>
+    <template v-else>
+        <input  :type="type"
+            :name="name"
+            :value="props.modelValue"
+            class="block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            :required="required"
+            :placeholder="label"
+            @input="emit('update:modelValue',$event.target.value)"
+        >
+    </template>
+
+    </div>
+</template>
---
+++ frontend/src/components/core/Modal.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { MinusCircleIcon } from '@heroicons/vue/24/outline'
+defineProps({
+  show: Boolean
+})
+</script>
+
+<template>
+  <div v-if="show" class="modal-wrapper">
+    <div class="modal">
+        <header>
+            <slot name="header">
+                <button class="" @click="$emit('close')"><MinusCircleIcon class="w-5 text-red-500" title="close"/></button>
+            </slot>
+        </header>
+        <hr class="mt-2 mb-3">
+      <div class="main">
+        <slot>default body</slot>
+      </div>
+      <footer class="modal-footer">
+        <slot name="footer">
+        </slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal-wrapper {
+  position: fixed;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.6);
+  display: grid;
+  place-items: center;
+}
+
+.modal {
+  background: white;
+  padding: 1rem;
+  width: 80vw;
+  max-width: 800px;
+  border-radius: 7px;
+}
+
+.modal-header {
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+  font-size: 1rem;
+}
+
+.modal-header button {
+  background: #ddd;
+  padding: 0.25rem 0.75rem;
+  border-radius: 20px;
+}
+
+.modal-header button:hover {
+  background: #c8c8c8;
+}
+</style>
